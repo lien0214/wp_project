f35f57d92b3fcba7a2fc83af43b0405976638bed
CRY666666 <96370801+CRY666666@users.noreply.github.com>
2023-01-02 20:23:28 +0800

    Update Seats.js

--- backend/src/routes/Seats.js
+++ backend/src/routes/Seats.js
@@ -2,59 +2,117 @@ import { Router } from "express";
 import SeatInfor from "../models/Seat";
 const router = Router();
 router.get("/account", async function (req, res) {
-    try {
-        const { account, password } = req.body;
-        const exist = await SeatInfor.findOne({ account });
+    const { account, password, ins } = req.query;
+    if(ins == 0){
+        const exist = await SeatInfor.findOne({ account, password });
         if (exist) {
           res.json({
-            message: `Your account has been registered!`,
-            valid: false
-          });
-        } else {
-          res.json({
+            message: "Login success!",
             valid: true
           });
+        } else {
+          const ext = await SeatInfor.findOne({ account });
+          if(ext){
+            res.json({
+              message: "Password incorrect, please try again.", 
+              valid: false
+            });
+          }
+          else {
+            res.json({
+              message: "Account not found!", 
+              valid: false
+            });
+          }
         }
-      } catch (e) {
-        res.json({ message: "Error" });
-      }  
+    }
+    else {
+      const exist = await SeatInfor.findOne({ account });
+      if (exist) {
+        res.json({
+          message: "Your account has been registered!",
+          valid: false
+        });
+      } else {
+        res.json({
+          valid: true
+        });
+      }
+    }
 })
 router.post("/infor", async function (req, res) {
     try {
-        const { account, password, position} = req.body;
-        const newSeat = new SeatInfor({ account, password, position });
-        newSeat.save();
-        res.json({
-          message: `Seat is rent succesfully by Account [${account}] !`,
-        });
+        const { account, password, wheretosit, seatID, ins} = req.body;
+        // console.log(account, password, wheretosit, seatID, ins);
+        if(seatID < 0){
+          if(ins === 0){
+            await SeatInfor.deleteOne({ account, password });
+            res.json({
+              message: "Your seat is returned successfully!"
+            });
+          }
+          else {
+            const infor = await SeatInfor.findOne({ account, password });
+            res.json({
+              message: "Seat found!",
+              whr: infor.wheretosit, 
+              pos: infor.seatID
+            });
+          }
+        }
+        else {
+            if(ins == 0){
+              const newSeat = new SeatInfor({ account, password, wheretosit, seatID });
+              newSeat.save();
+              res.json({
+                message: `Seat is rent succesfully by Account [${account}] !`
+              });
+            }
+            else {
+              const exist = await SeatInfor.findOne({ position });
+              if (exist) {
+                res.json({
+                  message: "The seat is rent!",
+                  pos: 1
+                });
+              }
+              else {
+                res.json({
+                  message: "The seat is empty.",
+                  pos: -1
+                });
+              }
+            }
+        }
       } catch (e) {
         res.json({ message: "Error" });
     }  
 })
-router.delete("/infor", async function (req, res) {
-    try {
-      const { account, password, ins } = req.body;
-      const exist = await SeatInfor.findOne({ account, password });
-      if (exist) {
-        if(ins){
-            res.json({
-            message: `Find your seat at position ${exist.position}`,
-            infor: { account, password, score },
-            });
-        }
-        else {
-            await SeatInfor.deleteOne({ account, password});
-            res.json({
-            message: "Your seat is returned successfully!"
-            });
-        }
-      } else {
-        res.json({
-          message: "Sorry, we didn't find your account.",
-        });
-      }
-    } catch (e) {
-      res.json({ message: "Error" });
-    }
-});
+// router.delete("/infor", async function (req, res) {
+//     try {
+//       const { account, password, ins } = req.query;
+//       // await SeatInfor.deleteMany({});
+//       const exist = await SeatInfor.findOne({ account, password });
+//       if (exist) {
+//         if(ins){
+//             res.json({
+//             message: `Find your seat at position ${exist.position}`,
+//             infor: { account, password, score },
+//             });
+//         }
+//         else {
+//             await SeatInfor.deleteOne({ account, password});
+//             res.json({
+//             message: "Your seat is returned successfully!"
+//             });
+//         }
+//       } else {
+//         res.json({
+//           message: "Account not found!",
+//         });
+//       }
+//     } catch (e) {
+//       res.json({ message: "Error" });
+//     }
+// });
 export default router;
