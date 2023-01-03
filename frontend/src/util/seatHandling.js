5b2607abced13d7f8c280e096d59f2e9cb99401f
lien0214 <ted20030214@gmail.com>
2023-01-03 21:45:55 +0800

    undebugged seatHandling lien

--- frontend/src/util/seatHandling.js
+++ frontend/src/util/seatHandling.js
@@ -1,9 +1,17 @@
 import {QryEmpty, SeatReturn, QryPosition} from './ApiFunc'
 const ShinGuan = 0, Lishin = 1;
 const rent = 0, ret = 1, search = 2;
-const seatHandling = (register, person) => {
+const seatHandling = async (register, person) => {
     let seat = [];
     let columnSize, rowSize, size; // size for total count of seats including all kind of seats
+    
+    if(register !== rent) {
+        const positionRet = await QryPosition(person.account, person.password);
+        console.log(positionRet);
+        person.wheretosit = positionRet.whr;
+        person.id = positionRet.pos;
+    }
+
     if(person.wheretosit === ShinGuan) {
         [columnSize, rowSize, size] = [20, 5, 96];
     }
@@ -28,37 +36,12 @@ const seatHandling = (register, person) => {
     // fill registered attribute
     for(let r = 0, id = 1; r < rowSize; r++) {
         for(let c = 0; c < columnSize && id <= size; c++, id++) {
-            if(!QryEmpty(person.wheretosit, id)) {
+            if(!(await QryEmpty(person.wheretosit, id))) {
                 seat[r][c].registered = true;
             }
         }
     }
-    // people.forEach(p => {
-    //     for(let r = 0, id = 1; r < rowSize; r++) {
-    //         for(let c = 0; c < columnSize && id <= size; c++, id++) {
-    //             if(p.seatID === seat[r][c].id) {
-    //                 seat[r][c].registered = true;
-    //                 seat[r][c].person = p;
-    //             }
-    //         }
-    //     }
-    // });
-    
-    // set return person seat
-    // if(register !== rent) {    //
-    //     for(let r = 0, id = 1; r < rowSize; r++) {
-    //         for(let c = 0; c < columnSize && id <= size; c++, id++) {
-    //             if(person.seatID === seat[r][c].id) {
-    //                 seat[r][c].ifReturn = true;
-    //             }
-    //         }
-    //     }
-    // }
-    if(register !== rent) {
-        const seatid = QryPosition(person.account, person.password);
-        console.log(seatid);
-        seat[seatid / columnSize][seatid % columnSize].ifReturn = true;
-    }
+    seat[person.id / columnSize][person.id % columnSize].ifReturn = true;
     
     return { rowSize, columnSize, size, seat };
 }
